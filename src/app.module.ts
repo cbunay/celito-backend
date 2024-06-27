ad32e0d97528131825cebe1d62d8d69f6bb98f52
Carlos Buñay <cbunay@outlook.com>
2024-06-27 16:05:58 -0500

    use env varriables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule,],
+  imports: [ConfigModule.forRoot(), UsersModule, AuthModule,],
   controllers: [AppController],
   providers: [],
 })
